83d00ab42d72308018ccabcf23588c5adedbcfb0
UtopiaBeam <utopiabeam@gmail.com>
2020-07-02 21:28:32 +0700

    ✨ Add connection to mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.mongoUrl,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }),
+    }),
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

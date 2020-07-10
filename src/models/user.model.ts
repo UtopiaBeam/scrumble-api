a7497a74ce8d50bdd5981f823d7aa47c7c93e51e
UtopiaBeam <utopiabeam@gmail.com>
2020-07-10 15:20:49 +0700

    🗃 Omit user password by default

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,7 +8,7 @@ export class User extends Document {
   @Prop({ required: true })
   username: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, select: false })
   password: string;
 
   @Prop({ required: true })
